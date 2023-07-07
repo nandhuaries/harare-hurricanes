ed4596fee5aa5a4403ab9964e0ff13a840397fde
nandhuaries <nandhu.s@ariesesolutions.com>
2023-07-07 18:05:30 +0530

    files

--- index.php
+++ index.php
@@ -1,29 +1,66 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
+<!-- my style -->
 
-<div class="hero overlay" 
-style="background-image: url('images/harare-home-banner-1.jpg');height:120vh;position:relative;">
-<div class="container">
-<div class="row align-items-center" >
+<!-- end my style -->
+
+<div class="hero overlay" style="background-image: url('images/harare-home-banner-1.jpg');height:120vh;position:relative;">
+    <div class="container">
+        <div class="row align-items-center">
 
 
 
-<div class="col-lg-5 ml-auto">
+            <div class="col-lg-5 ml-auto">
 
-<!-- <h1 class="text-white">Coming Soon</h1>
+                <!-- <h1 class="text-white">Coming Soon</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, molestias repudiandae pariatur.</p>
 <div id="date-countdown"></div>
 <p>
 <a href="#" class="btn btn-primary py-3 px-4 mr-3">Book Ticket</a>
 <a href="#" class="more light">Learn More</a>
-</p> --> 
+</p> -->
 
 
+            </div>
+        </div>
+    </div>
 </div>
+
+
+<!-- slider -->
+<!-- Swiper -->
+<div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </div>
+    </div>
+    <div class="swiper-pagination"></div>
 </div>
-</div>
-</div>
+<!-- end slider -->
 
 
-
-
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
