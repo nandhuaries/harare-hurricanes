59656f270deee26bbec7c7e74d2149e3cd375e2e
nandhuaries <nandhu.s@ariesesolutions.com>
2023-07-13 16:51:12 +0530

    styled the news page

--- press-releases.php
+++ press-releases.php
@@ -1,7 +1,7 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 
 <style type="text/css">
-	.hero .overlay:before {
+  .hero .overlay:before {
     content: "";
     position: absolute;
     background: rgba(0, 0, 0, 0.6);
@@ -9,183 +9,195 @@
     left: 0;
     right: 0;
     bottom: 0;
-}
+  }
 
-@keyframes fadeIn {
-  from {top: 20%; opacity: 0;}
-  to {top: 100; opacity: 1;}
-  
-}
+  @keyframes fadeIn {
+    from {
+      top: 20%;
+      opacity: 0;
+    }
 
-@-webkit-keyframes fadeIn {
-  from {top: 20%; opacity: 0;}
-  to {top: 100; opacity: 1;}
-  
-}
+    to {
+      top: 100;
+      opacity: 1;
+    }
 
-.wrapper {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  -webkit-transform: translate(-50%, -50%);
-  animation: fadeIn 1000ms ease;
-  -webkit-animation: fadeIn 1000ms ease;
-  
-}
+  }
 
+  @-webkit-keyframes fadeIn {
+    from {
+      top: 20%;
+      opacity: 0;
+    }
+
+    to {
+      top: 100;
+      opacity: 1;
+    }
+
+  }
+
+  .wrapper {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    animation: fadeIn 1000ms ease;
+    -webkit-animation: fadeIn 1000ms ease;
+
+  }
 </style>
 
 
 
-<div class="latest-news" >
-<div class="container" >
+<div class="latest-news">
+  <div class="container">
 
-<div class="row" style="margin-top:100px;">
-<div class="col-12 title-section">
-<h2 class="heading" style="font-size:30px;font-weight:600">Latest News</h2>
+    <div class="row" style="margin-top:100px;">
+      <div class="col-12 title-section">
+        <h2 class="heading" style="font-size:30px;font-weight:600">Latest News</h2>
+      </div>
+    </div>
+
+    <div class="row ">
+      <!-- no-gutters above div class -->
+
+
+      <div class="col-12 col-md-6 col-lg-4 mb-5 mb-lg-5 mb-md-5">
+        <div class="post-entry" style="background: black;margin-right: 1px;">
+          <a href="https://www.imdb.com/news/ni64129958/">
+            <!-- <img src="images/news/news1.png" alt="HARARE HURRICANES NEWS" class="img-fluid" style="height:300px;width: 100%;"> -->
+            <img src="images/news/newpic.webp" alt="HARARE HURRICANES NEWS" class="img-fluid" style="height:300px;width: 100%;">
+          </a>
+
+          <div class="caption" style="position:relative;margin-top:200px;">
+            <div class="caption-inner">
+              <a href="https://www.imdb.com/news/ni64129958/" target="_blank">
+                <h3 class="mb-3">Sanjay Dutt acquires Harare Hurricane with Sir Sohan Roy</h3>
+              </a>
+
+              <div class="author d-flex align-items-center">
+                <div class="text">
+                  <span>June 22, 2023</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-4 mb-5 mb-lg-5 mb-md-5 ">
+        <div class="post-entry" style="background: black;margin-right: 1px;">
+          <a href="https://www.crictracker.com/cricket-press-release/bollywood-star-sanjay-dutt-acquires-stake-in-zim-afro-t10-franchise-harare-hurricanes/">
+            <!-- <img src="images/news/news2-new.png" alt="HARARE HURRICANES NEWS" class="img-fluid" style="height:300px;width: 100%;"> -->
+            <img src="images/news/news2-new.png" alt="HARARE HURRICANES NEWS" class="img-fluid" style="height:300px;width: 100%;">
+          </a>
+
+          <div class="caption" style="position:relative;margin-top:200px;">
+            <div class="caption-inner">
+              <a href="https://www.crictracker.com/cricket-press-release/bollywood-star-sanjay-dutt-acquires-stake-in-zim-afro-t10-franchise-harare-hurricanes/" target="_blank">
+                <h3 class="mb-3">Bollywood star Sanjay Dutt acquires stake in Zim Afro T10 franchise Harare Hurricanes</h3>
+              </a>
+
+              <div class="author d-flex align-items-center">
+                <div class="text">
+                  <span>June 22, 2023</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <div class="col-12 col-md-6 col-lg-4 mb-5 mb-lg-5 mb-md-5 ">
+        <div class="post-entry" style="background: black;margin-right: 1px;">
+          <a href="http://dhunt.in/NlEYu">
+            <img src="images/news/new3-new.png" alt="HARARE HURRICANES NEWS" class="img-fluid" style="height:300px;width: 100%;">
+          </a>
+
+          <div class="caption" style="position:relative;margin-top:200px;">
+            <div class="caption-inner">
+              <a href="http://dhunt.in/NlEYu" target="_blank">
+                <h3 class="mb-3">Indians making their mark in Zimbabwe: Bollywood Icon Sanjay Dutt and Oscar-selected Hollywood Director Sir Sohan Roy Join Hands to Acquire</h3>
+              </a>
+
+              <div class="author d-flex align-items-center">
+                <div class="text">
+                  <span>June 21, 2023</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-4 mb-5 mb-lg-5 mb-md-5 ">
+        <div class="post-entry" style="background: black;margin-right: 1px;">
+          <a href="https://www.manoramaonline.com/sports/cricket/2023/06/23/sanjay-dutt-sohan-roy-acquire-harare-hurricanes-team-in-the-zim-afro-t-10.html">
+            <img src="images/news/news4-new.png" alt="HARARE HURRICANES NEWS" class="img-fluid" style="height:300px;width: 100%;">
+          </a>
+
+          <div class="caption" style="position:relative;margin-top:200px;">
+            <div class="caption-inner">
+              <a href="https://www.manoramaonline.com/sports/cricket/2023/06/23/sanjay-dutt-sohan-roy-acquire-harare-hurricanes-team-in-the-zim-afro-t-10.html" target="_blank">
+                <h3 class="mb-3">സിം ആഫ്രോ ടി10’ ടീമിനെ സ്വന്തമാക്കി സഞ്ജയ് ദത്തും സോഹൻ റോയിയും</h3>
+              </a>
+
+              <div class="author d-flex align-items-center">
+                <div class="text">
+                  <span>June 23, 2023</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <div class="col-12 col-md-6 col-lg-4 mb-5 mb-lg-5 mb-md-5 ">
+        <div class="post-entry" style="background: black;margin-right: 1px;">
+          <a href="https://24timemedia.com/sohan-roy-2023-cricket/">
+            <img src="images/news/news5-new.png" alt="HARARE HURRICANES NEWS" class="img-fluid" style="height:300px;width: 100%;">
+          </a>
+
+          <div class="caption" style="position:relative;margin-top:200px;">
+            <div class="caption-inner">
+              <a href="https://24timemedia.com/sohan-roy-2023-cricket/" target="_blank">
+                <h3 class="mb-3">സിംബാബ്വെയിലെ ‘ഹരാരെ ഹുറിക്കൻസ് ‘ ക്രിക്കറ്റ് ടീമിനെ ഏറ്റെടുത്ത് സഞ്ജയ് ദത്തും സോഹൻ റോയിയും.</h3>
+              </a>
+
+              <div class="author d-flex align-items-center">
+                <div class="text">
+                  <span>June 21, 2023</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+    </div>
+
+    
+
+
+  </div>
+
 </div>
-</div>
-
-<div class="row no-gutters">
 
 
-
-<div class="col-md-4">
-<div class="post-entry" style="background: black;margin-right: 1px;">
- <a href="https://www.imdb.com/news/ni64129958/">
- <img src="images/news/news1.png" alt="HARARE HURRICANES NEWS" 
-class="img-fluid" style="height:300px;width: 100%;">
-</a>
-
-<div class="caption" style="position:relative;margin-top:200px;">
-<div class="caption-inner">
-<a href="https://www.imdb.com/news/ni64129958/" target="_blank"><h3 class="mb-3">Sanjay Dutt acquires Harare Hurricane with Sir Sohan Roy</h3></a>
-
-<div class="author d-flex align-items-center">
-<div class="text">
-<span>June 22, 2023</span>
-</div>
-</div>
-</div>
-</div>
-
-</div>
-
-</div>
-
-<div class="col-md-4">
-<div class="post-entry" style="background: black;margin-right: 1px;">
- <a href="https://www.crictracker.com/cricket-press-release/bollywood-star-sanjay-dutt-acquires-stake-in-zim-afro-t10-franchise-harare-hurricanes/">
- <img src="images/news/news2.png" alt="HARARE HURRICANES NEWS" 
-class="img-fluid" style="height:300px;width: 100%;">
-</a>
-
-<div class="caption" style="position:relative;margin-top:200px;">
-<div class="caption-inner">
-<a href="https://www.crictracker.com/cricket-press-release/bollywood-star-sanjay-dutt-acquires-stake-in-zim-afro-t10-franchise-harare-hurricanes/" target="_blank"><h3 class="mb-3">Bollywood star Sanjay Dutt acquires stake in Zim Afro T10 franchise Harare Hurricanes</h3></a>
-
-<div class="author d-flex align-items-center">
-<div class="text">
-<span>June 22, 2023</span>
-</div>
-</div>
-</div>
-</div>
-
-</div>
-
-</div>
-
-
-<div class="col-md-4">
-<div class="post-entry" style="background: black;margin-right: 1px;">
- <a href="http://dhunt.in/NlEYu">
- <img src="images/news/news5.png" alt="HARARE HURRICANES NEWS" 
-class="img-fluid" style="height:300px;width: 100%;">
-</a>
-
-<div class="caption" style="position:relative;margin-top:200px;">
-<div class="caption-inner">
-<a href="http://dhunt.in/NlEYu" target="_blank"><h3 class="mb-3">Indians making their mark in Zimbabwe: Bollywood Icon Sanjay Dutt and Oscar-selected Hollywood Director Sir Sohan Roy Join Hands to Acquire</h3></a>
-
-<div class="author d-flex align-items-center">
-<div class="text">
-<span>June 21, 2023</span>
-</div>
-</div>
-</div>
-</div>
-
-</div>
-
-</div>
-
-
-
-</div>
-
-<div class="row no-gutters" style="margin-top:15px;">
-
-<div class="col-md-4">
-<div class="post-entry" style="background: black;margin-right: 1px;">
- <a href="https://www.manoramaonline.com/sports/cricket/2023/06/23/sanjay-dutt-sohan-roy-acquire-harare-hurricanes-team-in-the-zim-afro-t-10.html">
- <img src="images/news/news4.png" alt="HARARE HURRICANES NEWS" 
-class="img-fluid" style="height:300px;width: 100%;">
-</a>
-
-<div class="caption" style="position:relative;margin-top:200px;">
-<div class="caption-inner">
-<a href="https://www.manoramaonline.com/sports/cricket/2023/06/23/sanjay-dutt-sohan-roy-acquire-harare-hurricanes-team-in-the-zim-afro-t-10.html" target="_blank"><h3 class="mb-3">സിം ആഫ്രോ ടി10’ ടീമിനെ സ്വന്തമാക്കി സഞ്ജയ് ദത്തും സോഹൻ റോയിയും</h3></a>
-
-<div class="author d-flex align-items-center">
-<div class="text">
-<span>June 23, 2023</span>
-</div>
-</div>
-</div>
-</div>
-
-</div>
-
-</div>
-
-
-<div class="col-md-4">
-<div class="post-entry" style="background: black;margin-right: 1px;">
- <a href="https://24timemedia.com/sohan-roy-2023-cricket/">
- <img src="images/news/news6.png" alt="HARARE HURRICANES NEWS" 
-class="img-fluid" style="height:300px;width: 100%;">
-</a>
-
-<div class="caption" style="position:relative;margin-top:200px;">
-<div class="caption-inner">
-<a href="https://24timemedia.com/sohan-roy-2023-cricket/" target="_blank"><h3 class="mb-3">സിംബാബ്വെയിലെ ‘ഹരാരെ ഹുറിക്കൻസ് ‘ ക്രിക്കറ്റ് ടീമിനെ ഏറ്റെടുത്ത് സഞ്ജയ് ദത്തും സോഹൻ റോയിയും.</h3></a>
-
-<div class="author d-flex align-items-center">
-<div class="text">
-<span>June 21, 2023</span>
-</div>
-</div>
-</div>
-</div>
-
-</div>
-
-</div>
-
-
-
-
-
-
-</div>
-
-
-</div>
-
-</div>
-
-
-<?php include 'footer.php';?>
-
+<?php include 'footer.php'; ?>
